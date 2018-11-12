1fcc11470821196451d254e77d3e8f27b3a23154
Victoria B <viktorija.bourassa@gmail.com>
2018-11-12 14:12:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,14 +8,7 @@ function takeANumber(katzDeliLine, name){// this function should return a new cu
 
 takeANumber(katzDeliLine, "Ada");
 
-function currentLine(katzDeliLine){
-  //returns everybody in line
-  if (katzDeliline.length === 0){
-    return "The line is currently empty";
-    }else{
-    return "The line is currently: 1. " + katzDeliLine[0] + "2." + katzDeliLine[1];
-  }
-}
+
 function nowServing(katzDeliLine){
   var katzDeliLine = ["Steven"];// should return the first person in line and then remove him/her from the line.
   if (katzDeliLine.length===0){//we are determining if there are people in line
@@ -24,4 +17,12 @@ function nowServing(katzDeliLine){
   //remove the first person from line and return that person at the same time
   return 'Currently serving ' + katzDeliLine[0];
 }
+}
+function currentLine(katzDeliLine){
+  //returns everybody in line
+  if (katzDeliline.length === 0){
+    return "The line is currently empty";
+    }else{
+    return "The line is currently: 1. " + katzDeliLine[0] + "2." + katzDeliLine[1];
+  }
 }
